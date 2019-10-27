88896133b0ecacbda04b202dc24bb6d5e6886ef3
Shivam Balwani <shivambalwani0407@gmail.com>
2019-10-27 11:55:58 -0700

    Http 404 Raised

--- music/views.py
+++ music/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
 from .models import *
 from django.template import loader
 
 def index(request):
     all_albums=Album.objects.all()
     #template=loader.get_template('music/index.html')
-    context={
-        'all_albums':all_albums,
-    }
+    context={'all_albums':all_albums,}
     #return HttpResponse(template.render(context,request))
     return render(request,'music/index.html',context)
 
-def details(request ,album_id):
-    return HttpResponse("Album Request=>" + str(album_id) )
+#def details(request ,album_id):
+    #return HttpResponse("Album Request=>" + str(album_id) )
+
+#raising a Http404 Error
+def details(request,album_id):
+    try:
+        album=Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album Does not exist")
+    return render(request,"music/details.html",{'album':album})
 # Create your views here.
