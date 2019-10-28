15ddfaa4c5ef02dbebe346a28efd96cc5175ab27
Shivam Balwani <shivambalwani0407@gmail.com>
2019-10-28 05:45:58 -0700

    form template view function

--- music/models.py
+++ music/models.py
@@ -12,6 +12,7 @@ class Song(models.Model):
     album=models.ForeignKey(Album,on_delete=models.CASCADE)
     file_type=models.CharField(max_length=10)
     song_title=models.CharField(max_length=250)
+    is_favourite=models.BooleanField(default=False)
     def __str__(self):
         return self.file_type +'-'+ self.song_title
 # Create your models here.
